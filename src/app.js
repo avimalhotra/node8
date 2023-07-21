326bc6c1afaffe3ba407a4eded53a49e9ebc7136
avimalhotra <avimalhotra505@gmail.com>
2023-07-21 21:28:39 +0530

    Express JS

--- src/app.js
+++ src/app.js
@@ -3,22 +3,43 @@ require("dotenv").config();
 const port=process.env.PORT;
 
 const app=express();
+//app.use(express.static('src/public'));
+//app.use(express.static('node_modules/bootstrap/dist'));
 
-app.use((req,res, next)=>{
+/* app.use((req,res, next)=>{
     console.log(`Login at ${new Date().toLocaleString()}`);
     next();
-});
+}); */
+
+/* app.use('/user',(req,res, next)=>{
+    console.log(`User Login at ${new Date().toLocaleString()}`);
+    next();
+}); */
+
 
 app.get("/",(req,res)=>{
     res.setHeader('Content-Type','text/html');
-    res.status(200).send("Homepage");
+   res.status(200).send("<h1>Home Page</h1>");
+    //res.status(200).send(req.url);
+    //res.status(200).send(req.query);
+    //res.status(200).json({"key":req.query});
 });
 
-
-app.get("/app",(req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    res.status(200).send("Hello App");
+app.get("/product/",(req,res)=>{
+    res.status(200).send("Product page");
 });
+app.get("/product/:brand/",(req,res)=>{
+    res.status(200).send(req.params);
+});
+app.get("/product/:brand/:product",(req,res)=>{
+    res.status(200).send(req.params);
+});
+
+app.post("/submit",(req,res)=>{
+    console.log("data submitted");
+    res.status(200).send("submitted");
+});
+
 
 
 
@@ -30,4 +51,4 @@ app.get("/**",(req,res)=>{
 
 app.listen(port,()=>{
     console.log(`server running at http://127.0.0.1:${port}`);
-})
+});
